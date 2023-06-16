692c7524460b54a896f572fe8c6461f63fa3f6b4
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-16 14:23:51 -0700

    nn

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,2 +1,24 @@
 #include<stdio.h>
 
+/**
+ * main - entry point
+ *
+ * description: this program for write the alpgabit letters
+ *
+ * return: always  0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= z);
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
