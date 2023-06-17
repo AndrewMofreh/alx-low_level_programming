12ad1c28ade463c9c4f7a2cc3733d53db249a9cd
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-17 02:59:11 -0700

    nn

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,2 +1,20 @@
 #include<stdio.h>
 
+/**
+ * main - entry point
+ *
+ * discreption: its a c program for printing integer from 0 to 9
+ *
+ * Return: 0 (success)
+*/
+
+	int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
+	}
+	return (0);
+}
