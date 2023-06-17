f4318e31535944fd19c4d0114061533b67510f07
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-17 00:41:31 -0700

    we used for and if then the excuted action

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,2 +1,22 @@
 #include<stdio.h>
 
+/**
+ * main - entry point
+ *
+ * description: this program is for printing alphabt wzout letter e & q
+ *
+ * Return: 0 (success)
+*/
+
+
+
+
+
+
+	int main(void)
+{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	if (ch != 'e' && ch != 'q')
+	{ putchar(ch); }
+	return (0);
+}
