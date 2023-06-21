7be5da75f9947d014e88792a7ee8fe9429c91d4d
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-21 13:17:37 -0700

    nn

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,13 @@
 
 int print_last_digit(int n)
 {
-	if (n > 0)
-	n =  n % 10;
+	int last_digit;
+
+	if (n < 0)
+	last_digit = abs(n) % 10;
 	else
-	n = abs(n) % 10;
-		return (n);
+	last_digit  = n % 10;
+
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
