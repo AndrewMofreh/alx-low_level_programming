6c1f258dbc760248a38d5ea38c3bd12070849b35
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-21 05:49:27 -0700

    nn

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,19 @@
 #include<stdio.h>
+#include<main.h>
 
+/**
+ * print_alphabet - it is a function for printing alphabet
+*/
+
+
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+
+}
