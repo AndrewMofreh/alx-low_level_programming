095d78e259425d7db8fde1bc5f1731718c166e99
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-21 11:00:59 -0700

    nn

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,2 +1,20 @@
 #include<stdio.h>
+#include"main.h"
 
+/**
+ * _islower - its for return 0 if its a lower case
+ *
+ * @c: check the input of the finction
+ *
+ * Return: 1 (success)
+ * 0 (not success)
+*/
+
+int _islower(int c)
+{
+	if (c > 97 && c <= 122)
+	{
+		return (1);
+		return (0);
+	}
+}
