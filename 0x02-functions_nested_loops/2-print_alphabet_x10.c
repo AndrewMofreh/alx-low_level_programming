38072da2b917a335b30b10dfcc8ea65c4fd820ab
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-21 09:57:42 -0700

    nn

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,23 @@
 #include<stdio.h>
+#include"main.h"
 
+/**
+ * print_alphabet_x10 - its for printing alphabet 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char c;
+
+		for (c = 'A'; c <= 'Z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
