7291079a5a4cb9e45951a7e0d3c6e96f0bf4385a
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-21 14:44:35 -0700

    need to be revised

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_to_98 - print num to 98 7aga kdh
+ *
+ * @n: output
+*/
+
+void print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+		for (count = n; count > 98; count--)
+			printf("%d, ", count);
+	els
+		for (count = n; count < 98; count++)
+			printf("%d, ", count);
+	printf("98\n");
+}
+
