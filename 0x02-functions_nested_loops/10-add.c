bdb255094c184bea2471cbbe9d57117306874d32
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-21 14:34:09 -0700

    nn

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+#include<stdlib.h>
+
+/**
+ *add - add 2 integer
+ * @a: 1st input
+ * @b: 2nd input
+ *
+ * Return: sum of both
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
