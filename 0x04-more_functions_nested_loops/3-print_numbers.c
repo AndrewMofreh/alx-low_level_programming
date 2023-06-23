d0a64f05c558504d0cad4979a12b34d69c6b2dc8
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-23 06:16:38 -0700

    nn

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * print_number  - its for return 0 if its a lower case
+ *
+ * discription: its a function to print from 0 to 9
+ *
+ * Return: 1 (success)
+ * 0 (not success)
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= 0)
+	{
+		_putchar (i);
+	}
+	_putchar('\n')
+
+}
+
