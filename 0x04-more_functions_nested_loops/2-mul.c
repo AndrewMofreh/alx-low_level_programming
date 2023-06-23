fcc9cba39eadd83319eb7c5e7ffc4979e085878b
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-23 05:59:03 -0700

    nn

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * mul - its for return 0 if its a lower case
+ *
+ * @a: check the input of the function
+ *
+ * @b: input for the function
+ *
+ * Return: 1 (success)
+ * 0 (not success)
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
