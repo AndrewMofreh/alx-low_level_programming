9313c53e2e42c3d47ef3d1ce91f74d0c7c78b679
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-23 07:42:46 -0700

    nn

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * more_numbers - its for return 0 if its a lower case
+ *
+ * discription: its a function to print from 0 to 14 ten times
+ *
+*/
+
+void more_numbers(void)
+{
+
+	int i = 0;
+	int c = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			_putchar(c + '0');
+		}
+	_putchar('\n');
+	}
+}
