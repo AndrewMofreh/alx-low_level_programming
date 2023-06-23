58fa10cd55347beba126ee0e4aca26e2d502e870
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-23 05:30:05 -0700

    nn

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _isalpha - its for return 0 if its a lower case
+ *
+ * @c: check the input of the function
+ *
+ * Return: 1 (success)
+ * 0 (not success)
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	return (0);
+}
+
