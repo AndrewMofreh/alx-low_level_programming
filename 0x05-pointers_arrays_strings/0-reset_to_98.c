b83c5d227ef1c72cb073afba59f14542090984cf
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-26 11:04:59 -0700

    nn

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * reset_to_98 - its a function that assign a new value to a pointer named n
+ *
+ * @n: pointer to an int
+ *
+ * Return: n
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
