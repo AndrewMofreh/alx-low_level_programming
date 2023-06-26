18d4e34fea7e184eb6d55a75744a3fb635c7593b
Andrewmofreh <Andrewmofreh@gmail.com>
2023-06-26 11:19:36 -0700

    ..

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * swap_int - its a function that assign a new value to a pointer named n
+ *
+ * @a: pointer to an int
+ * @b: pointer to an int
+ *
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
